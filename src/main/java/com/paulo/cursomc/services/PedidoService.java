ef66e3f25e384602ca2e60b150b067de4f2ca48e
Paulo Ferreira <mrpauloii@gmail.com>
2018-06-24 11:42:14 -0300

    Enderpoint

---
+++ src/main/java/com/paulo/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.paulo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulo.cursomc.domain.Pedido;
+import com.paulo.cursomc.repositories.PedidoRepository;
+import com.paulo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() ->
+			new ObjectNotFoundException("Objeto não encontrado ! ID: " + id + ", Tipo: "
+					+ Pedido.class.getName()
+		));
+	}
+}
