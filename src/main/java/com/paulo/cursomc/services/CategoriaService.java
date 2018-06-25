8aa6422286d2b410f30a99376f117228b9b2c52e
Paulo Ferreira <mrpauloii@gmail.com>
2018-06-25 09:57:29 -0300

    Pageinção com paramentros opcionais na requisição

--- src/main/java/com/paulo/cursomc/services/CategoriaService.java
+++ src/main/java/com/paulo/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.paulo.cursomc.domain.Categoria;
@@ -15,9 +18,11 @@ import com.paulo.cursomc.services.exceptions.ObjectNotFoundException;
 @Service
 public class CategoriaService {
 
+	// Repositorio - Onde Salva/Delete
 	@Autowired
 	private CategoriaRepository repo;
 	
+	// Mostrar (uma) Categoria
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() ->
@@ -26,20 +31,24 @@ public class CategoriaService {
 		));
 	}
 	
+	// Mostrar Categorias
 	public List<Categoria> findAll(){
 		return repo.findAll();
 	}
 	
+	//Para Inserir Categoria
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
 	
+	// Para Atulizar Categoria
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	} 
 	
+	//Para poder deletar Cadegorias
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -48,5 +57,10 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não e possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer LinesPerPage,String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, LinesPerPage,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
