aa4b638e07ba8cac07e20fc6bb3dd6d4808983db
Paulo Ferreira <mrpauloii@gmail.com>
2018-07-01 18:10:46 -0300

    Validação Update Cliente

--- src/main/java/com/paulo/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/paulo/cursomc/dto/ClienteNewDTO.java
@@ -2,22 +2,47 @@ package com.paulo.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.paulo.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 	// Dados Pessoais
+	@NotEmpty(message="Preenchimento obrigario")
+	@Length(min=5,max=120,message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigario")
+	@Email(message="Email Invalido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigario")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
 	// Endereço
+	@NotEmpty(message="Preenchimento obrigario")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigario")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigario")
 	private String cep;
 	
 	// Telefone
+	@NotEmpty(message="Preenchimento obrigario")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3; 
---
+++ src/main/java/com/paulo/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,50 @@
+package com.paulo.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.paulo.cursomc.domain.Cliente;
+import com.paulo.cursomc.domain.enums.TipoCliente;
+import com.paulo.cursomc.dto.ClienteNewDTO;
+import com.paulo.cursomc.repositories.ClienteRepository;
+import com.paulo.cursomc.resources.exception.FieldMessage;
+import com.paulo.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
+		}
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("Email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
