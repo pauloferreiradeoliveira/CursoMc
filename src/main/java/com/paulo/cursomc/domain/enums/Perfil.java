9d0499c9b93f7f7121c03fc69038626482c970d0
Paulo Ferreira <mrpauloii@gmail.com>
2018-07-11 15:40:08 -0300

    Implementando Autorização

--- src/main/java/com/paulo/cursomc/domain/enums/Perfil.java
+++ src/main/java/com/paulo/cursomc/domain/enums/Perfil.java
@@ -25,7 +25,6 @@ public enum Perfil {
 			return null;
 		}
 		for (Perfil perfil : Perfil.values()) {
-			System.out.println(perfil);
 			if(cod.equals(perfil.getCodigo())) {
 				return perfil;
 			}
